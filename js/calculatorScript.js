d887312411af02e9906a350e68a0b954710826bf
Liamine D. <liamine14@hotmail.fr>
2025-07-17 18:26:15 +0000

    back to jsx

--- js/calculatorScript.js
+++ js/calculatorScript.js
@@ -1,5 +1,10 @@
 "use strict";
 
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+var _react = _interopRequireWildcard(require("react"));
+var _client = _interopRequireDefault(require("react-dom/client"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+function _interopRequireWildcard(e, t) { if ("function" == typeof WeakMap) var r = new WeakMap(), n = new WeakMap(); return (_interopRequireWildcard = function _interopRequireWildcard(e, t) { if (!t && e && e.__esModule) return e; var o, i, f = { __proto__: null, "default": e }; if (null === e || "object" != _typeof(e) && "function" != typeof e) return f; if (o = t ? n : r) { if (o.has(e)) return o.get(e); o.set(e, f); } for (var _t in e) "default" !== _t && {}.hasOwnProperty.call(e, _t) && ((i = (o = Object.defineProperty) && Object.getOwnPropertyDescriptor(e, _t)) && (i.get || i.set) ? o(f, _t, i) : f[_t] = e[_t]); return f; })(e, t); }
 function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
@@ -7,10 +12,10 @@ function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length)
 function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
 function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
 function JsCalculator() {
-  var _React$useState = React.useState("0"),
-    _React$useState2 = _slicedToArray(_React$useState, 2),
-    display = _React$useState2[0],
-    setDisplay = _React$useState2[1];
+  var _useState = (0, _react.useState)("0"),
+    _useState2 = _slicedToArray(_useState, 2),
+    display = _useState2[0],
+    setDisplay = _useState2[1];
   var displayNum = function displayNum(num) {
     setDisplay(function (prev) {
       if (prev === "0" || prev === "Erreur") {
@@ -36,100 +41,101 @@ function JsCalculator() {
   var equalBtn = function equalBtn() {
     try {
       var expression = display.replace(/x/g, "*");
+      // Attention à eval, à sécuriser en prod
       var result = eval(expression);
       setDisplay(result.toString());
     } catch (_unused) {
       setDisplay("Erreur");
     }
   };
-  return React.createElement("div", {
+  return /*#__PURE__*/_react["default"].createElement("div", {
     id: "container"
-  }, React.createElement("div", {
+  }, /*#__PURE__*/_react["default"].createElement("div", {
     id: "display"
-  }, display), React.createElement("div", {
+  }, display), /*#__PURE__*/_react["default"].createElement("div", {
     id: "calculator"
-  }, React.createElement("button", {
+  }, /*#__PURE__*/_react["default"].createElement("button", {
     id: "equals",
     onClick: equalBtn
-  }, "="), React.createElement("button", {
+  }, "="), /*#__PURE__*/_react["default"].createElement("button", {
     id: "add",
     onClick: function onClick() {
       return displayNum("+");
     }
-  }, "+"), React.createElement("button", {
+  }, "+"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "subtract",
     onClick: function onClick() {
       return displayNum("-");
     }
-  }, "-"), React.createElement("button", {
+  }, "-"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "multiply",
     onClick: function onClick() {
       return displayNum("x");
     }
-  }, "x"), React.createElement("button", {
+  }, "x"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "divide",
     onClick: function onClick() {
       return displayNum("/");
     }
-  }, "/"), React.createElement("button", {
+  }, "/"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "decimal",
     onClick: function onClick() {
       return displayNum(".");
     }
-  }, "."), React.createElement("button", {
+  }, "."), /*#__PURE__*/_react["default"].createElement("button", {
     id: "clear",
     onClick: clearBtn
-  }, "Clear"), React.createElement("button", {
+  }, "Clear"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "zero",
     onClick: function onClick() {
       return displayNum("0");
     }
-  }, "0"), React.createElement("button", {
+  }, "0"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "one",
     onClick: function onClick() {
       return displayNum("1");
     }
-  }, "1"), React.createElement("button", {
+  }, "1"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "two",
     onClick: function onClick() {
       return displayNum("2");
     }
-  }, "2"), React.createElement("button", {
+  }, "2"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "three",
     onClick: function onClick() {
       return displayNum("3");
     }
-  }, "3"), React.createElement("button", {
+  }, "3"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "four",
     onClick: function onClick() {
       return displayNum("4");
     }
-  }, "4"), React.createElement("button", {
+  }, "4"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "five",
     onClick: function onClick() {
       return displayNum("5");
     }
-  }, "5"), React.createElement("button", {
+  }, "5"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "six",
     onClick: function onClick() {
       return displayNum("6");
     }
-  }, "6"), React.createElement("button", {
+  }, "6"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "seven",
     onClick: function onClick() {
       return displayNum("7");
     }
-  }, "7"), React.createElement("button", {
+  }, "7"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "eight",
     onClick: function onClick() {
       return displayNum("8");
     }
-  }, "8"), React.createElement("button", {
+  }, "8"), /*#__PURE__*/_react["default"].createElement("button", {
     id: "nine",
     onClick: function onClick() {
       return displayNum("9");
     }
   }, "9")));
 }
-var root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(React.createElement(JsCalculator));
+var root = _client["default"].createRoot(document.getElementById("app"));
+root.render(/*#__PURE__*/_react["default"].createElement(JsCalculator, null));
--- src/calculatorScript.jsx
+++ src/calculatorScript.jsx
@@ -1,63 +1,67 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+
 function JsCalculator() {
-  const [display, setDisplay] = React.useState("0");
+  const [display, setDisplay] = useState("0")
 
   const displayNum = (num) => {
-    setDisplay((prev) => {
+    setDisplay(prev => {
       if (prev === "0" || prev === "Erreur") {
-        return /[0-9.]/.test(num) ? num : "0" + num;
+        return /[0-9.]/.test(num) ? num : "0" + num
       }
-      const regexOp = /[+\-x*/]/;
+      const regexOp = /[+\-x*/]/
       if (num === ".") {
-        const parts = prev.split(/[\+\-\*/x]/);
-        const lastPart = parts[parts.length - 1];
-        if (lastPart.includes(".")) return prev;
+        const parts = prev.split(/[\+\-\*/x]/)
+        const lastPart = parts[parts.length - 1]
+        if (lastPart.includes(".")) return prev
       }
       if (regexOp.test(prev.slice(-1)) && regexOp.test(num)) {
-        if (num === "-") return prev + num;
-        if (regexOp.test(prev.slice(-2, -1))) return prev.slice(0, -2) + num;
-        return prev.slice(0, -1) + num;
+        if (num === "-") return prev + num
+        if (regexOp.test(prev.slice(-2, -1))) return prev.slice(0, -2) + num
+        return prev.slice(0, -1) + num
       }
-      return prev + num;
-    });
-  };
+      return prev + num
+    })
+  }
 
-  const clearBtn = () => setDisplay("0");
+  const clearBtn = () => setDisplay("0")
 
   const equalBtn = () => {
     try {
-      const expression = display.replace(/x/g, "*");
-      const result = eval(expression);
-      setDisplay(result.toString());
+      const expression = display.replace(/x/g, "*")
+      const result = eval(expression)
+      setDisplay(result.toString())
     } catch {
-      setDisplay("Erreur");
+      setDisplay("Erreur")
     }
-  };
+  }
 
   return (
-    React.createElement("div", { id: "container" },
-      React.createElement("div", { id: "display" }, display),
-      React.createElement("div", { id: "calculator" },
-        React.createElement("button", { id: "equals", onClick: equalBtn }, "="),
-        React.createElement("button", { id: "add", onClick: () => displayNum("+") }, "+"),
-        React.createElement("button", { id: "subtract", onClick: () => displayNum("-") }, "-"),
-        React.createElement("button", { id: "multiply", onClick: () => displayNum("x") }, "x"),
-        React.createElement("button", { id: "divide", onClick: () => displayNum("/") }, "/"),
-        React.createElement("button", { id: "decimal", onClick: () => displayNum(".") }, "."),
-        React.createElement("button", { id: "clear", onClick: clearBtn }, "Clear"),
-        React.createElement("button", { id: "zero", onClick: () => displayNum("0") }, "0"),
-        React.createElement("button", { id: "one", onClick: () => displayNum("1") }, "1"),
-        React.createElement("button", { id: "two", onClick: () => displayNum("2") }, "2"),
-        React.createElement("button", { id: "three", onClick: () => displayNum("3") }, "3"),
-        React.createElement("button", { id: "four", onClick: () => displayNum("4") }, "4"),
-        React.createElement("button", { id: "five", onClick: () => displayNum("5") }, "5"),
-        React.createElement("button", { id: "six", onClick: () => displayNum("6") }, "6"),
-        React.createElement("button", { id: "seven", onClick: () => displayNum("7") }, "7"),
-        React.createElement("button", { id: "eight", onClick: () => displayNum("8") }, "8"),
-        React.createElement("button", { id: "nine", onClick: () => displayNum("9") }, "9")
-      )
-    )
-  );
+    <div id="container">
+      <div id="display">{display}</div>
+      <div id="calculator">
+        <button id="equals" onClick={equalBtn}>=</button>
+        <button id="add" onClick={() => displayNum("+")}>+</button>
+        <button id="subtract" onClick={() => displayNum("-")}>-</button>
+        <button id="multiply" onClick={() => displayNum("x")}>x</button>
+        <button id="divide" onClick={() => displayNum("/")}>/</button>
+        <button id="decimal" onClick={() => displayNum(".")}>.</button>
+        <button id="clear" onClick={clearBtn}>Clear</button>
+
+        <button id="zero" onClick={() => displayNum("0")}>0</button>
+        <button id="one" onClick={() => displayNum("1")}>1</button>
+        <button id="two" onClick={() => displayNum("2")}>2</button>
+        <button id="three" onClick={() => displayNum("3")}>3</button>
+        <button id="four" onClick={() => displayNum("4")}>4</button>
+        <button id="five" onClick={() => displayNum("5")}>5</button>
+        <button id="six" onClick={() => displayNum("6")}>6</button>
+        <button id="seven" onClick={() => displayNum("7")}>7</button>
+        <button id="eight" onClick={() => displayNum("8")}>8</button>
+        <button id="nine" onClick={() => displayNum("9")}>9</button>
+      </div>
+    </div>
+  )
 }
 
-const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(React.createElement(JsCalculator));
+const root = ReactDOM.createRoot(document.getElementById('app'))
+root.render(<JsCalculator />)
