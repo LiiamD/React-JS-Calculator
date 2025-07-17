1c5a5510fa9fcc83509004d3b98a124b94a39a00
Liamine D. <liamine14@hotmail.fr>
2025-07-17 13:33:39 +0000

    delete

--- react-js-calculator/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import JsCalculator from "./JsCalculator";  // si ton composant est dans ce fichier
-
-function App() {
-  return <JsCalculator />;
-}
-
-export default App;
